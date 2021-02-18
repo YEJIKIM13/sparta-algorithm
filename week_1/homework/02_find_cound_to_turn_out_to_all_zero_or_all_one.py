deab6365e46eb71a62161c0798a1536e73009ac8
YEJIKIM13 <lerry597@gmail.com>
2021-02-18 16:35:15 +0900

    다른 방식으로 해결(그리디)

--- week_1/homework/02_find_cound_to_turn_out_to_all_zero_or_all_one.py
+++ week_1/homework/02_find_cound_to_turn_out_to_all_zero_or_all_one.py
@@ -2,22 +2,11 @@ input = "011110"
 
 
 def find_count_to_turn_out_to_all_zero_or_all_one(string):
-    count_make_all_zero = 0
-    count_make_all_one = 0
-
-    if string[0] == '0':
-        count_make_all_one += 1
-    elif string[0] == '1':
-        count_make_all_zero += 1
-
-    for i in range(len(string) - 1):
-        if string[i] != string[i+1]:
-            if string[i+1] == '0':
-                count_make_all_one += 1
-            if string[i+1] == '1':
-                count_make_all_zero += 1
-
-    return min(count_make_all_zero, count_make_all_one)
+    count = 0
+    for index in range(len(string)-1):
+        if string[index] != string[index +1]:
+            count += 1
+    return (count + 1)  //  2
 
 
 result = find_count_to_turn_out_to_all_zero_or_all_one(input)
