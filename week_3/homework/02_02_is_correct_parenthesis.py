02936bc7c8fc6103212bbd2aa4c473979dfb7102
YEJIKIM13 <lerry597@gmail.com>
2021-03-08 06:57:24 +0900

    올바른 괄호 해설강의

--- week_3/homework/02_02_is_correct_parenthesis.py
+++ week_3/homework/02_02_is_correct_parenthesis.py
@@ -2,8 +2,20 @@ s = "(())()"
 
 
 def is_correct_parenthesis(string):
-    # 구현해보세요!
-    return
+    stack = []
+
+    for i in range(len(string)):
+        if string[i] == "(":
+            stack.append(i)  # 아무런 값 이 들어가도 상관 없음! ( 가 들어갔는지 여부만 저장해 둔 거니까~!
+        elif string[i] == ")":
+            if len(stack) == 0:
+                return False
+            stack.pop()
+
+    if len(stack) != 0:
+        return False
+    else:
+        return True
 
 
 print(is_correct_parenthesis(s))  # True 를 반환해야 합니다!
