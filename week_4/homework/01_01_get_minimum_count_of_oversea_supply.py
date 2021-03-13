261a63da77f403166c0e210d4780093003b2ee6f
YEJIKIM13 <lerry597@gmail.com>
2021-03-13 18:20:50 +0900

    Update 01_01_get_minimum_count_of_oversea_supply.py

--- week_4/homework/01_01_get_minimum_count_of_oversea_supply.py
+++ week_4/homework/01_01_get_minimum_count_of_oversea_supply.py
@@ -5,6 +5,8 @@ supply_dates = [4, 10, 15]
 supply_supplies = [20, 5, 10]
 supply_recover_k = 30
 
+# 힙 - 최댓값, 최솟값 뽑아낼 때 유리!!!
+
 
 def get_minimum_count_of_overseas_supply(stock, dates, supplies, k):
     # 풀어보세요!
